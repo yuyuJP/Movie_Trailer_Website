918dee7d211cabcb74dab9ea3049edf3ea06c36e
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-03-22 13:22:48 +0900

    Made a few changes to meet the specifications of PEP8.

--- media.py
+++ media.py
@@ -1,11 +1,21 @@
 import webbrowser
 
+
 class Movie():
-    """This class provides a way to store movie related information"""
-    
+    """
+    This class provides a way to store movie related information
+
+    Attributes:
+        title(str): Movie title.
+        storyline(str): Storyline of a movie.
+        poster_image_url(str): Poster image link of a movie.
+        trailer_youtube_url(str): Youtube video link of a movie's trailer.
+    """
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
